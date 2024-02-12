41e2d7c0984ef9909d3442879c28034f1364d7bf
Marian Basti <31198560+marianbasti@users.noreply.github.com>
2024-02-12 18:08:52 +0000

    Cleanup

--- update_hf_dataset.py
+++ update_hf_dataset.py
@@ -3,17 +3,8 @@ import subprocess
 from dataset_utils import update
 from scrapper import Scrapper
 
-# Configuration
-HF_USER = os.getenv('HF_USER')
-HF_DATASET = os.getenv('HF_DATASET')
-REPO_PATH = f"{HF_USER}/{HF_DATASET}"
-DATASET_FILE = 'boletin-oficial-argentina.jsonl'
+# ConfigurationDATASET_FILE = 'boletin-oficial-argentina.jsonl'
 
 # Update the dataset
 scrapper = Scrapper()
 update(scrapper, DATASET_FILE)
-
-# Add and commit changes
-#subprocess.run(['git', 'add', DATASET_FILE], check=True)
-#subprocess.run(['git', 'commit', '-m', 'Update dataset'], check=True)
-# Commented because pushing is handled by the GitHub Action itself.
